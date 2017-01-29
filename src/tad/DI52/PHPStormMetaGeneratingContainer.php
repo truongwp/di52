6fd9a0f9db09f9fd634807e439c3a1a8fb4fdb62
Luca Tumedei <luca@theaveragedev.com>
2017-01-29 10:48:02 +0100

    updated to generate new syntax

--- src/tad/DI52/PHPStormMetaGeneratingContainer.php
+++ src/tad/DI52/PHPStormMetaGeneratingContainer.php
@@ -11,19 +11,7 @@ class tad_DI52_PHPStormMetaGeneratingContainer extends \tad_DI52_Container {
 	public function make( $classOrInterface ) {
 		$made = parent::make( $classOrInterface );
 
-		if ( class_exists( $classOrInterface ) ) {
-			$classOrInterfaceReflection = new ReflectionClass( $classOrInterface );
-			$classOrInterfaceClass = $classOrInterfaceReflection->getName();
-		} else {
-			$classOrInterfaceClass = $classOrInterface;
-		}
-
-		$implementationReflection = new ReflectionClass( $made );
-		$implementationClass = $implementationReflection->getName();
-
-		$classOrInterfaceClass = '\\' . ltrim( $classOrInterfaceClass, '\\' );
-
-		$this->made[ $classOrInterfaceClass ] = $implementationClass;
+		$this->registerMade( $classOrInterface, $made );
 
 		return $made;
 	}
@@ -47,13 +35,20 @@ class tad_DI52_PHPStormMetaGeneratingContainer extends \tad_DI52_Container {
 
 		$template = <<<PHP
 <?php
-
 namespace PHPSTORM_META {
-	\$STATIC_METHOD_TYPES = [
-		\\tad_DI52_Container::make( '' ) => [
+
+	override( \\tad_DI52_Container::make(0),
+	  map( [ //map of argument value -> return type
 			{{bindings}}
-		]
-	];
+		])
+	);
+
+	// support ArrayAccess ['key'] like calls
+	override(new \\tad_DI52_Container,
+		map([ //map of argument value -> return type
+			{{bindings}}
+		])
+	);
 }
 
 PHP;
@@ -61,11 +56,46 @@ PHP;
 		$bindings = array();
 
 		foreach ( $this->made as $key => $implementation ) {
-			$bindings[] = "{$key} instanceof {$implementation}";
+			$bindings[] = "{$key} => {$implementation}";
 		}
 
 		$contents = str_replace( '{{bindings}}', implode( ",\n\t\t\t", $bindings ), $template );
 
 		file_put_contents( $destination, $contents );
+	}
+
+	public function offsetGet( $offset ) {
+		$made = parent::offsetGet( $offset );
+
+		$this->registerMade( $offset, $made );
+
+		return $made;
+	}
+
+	/**
+	 * @param $classOrInterface
+	 * @param $made
+	 */
+	protected function registerMade( $classOrInterface, $made ) {
+		if ( class_exists( $classOrInterface ) ) {
+			$classOrInterfaceReflection = new ReflectionClass( $classOrInterface );
+			$classOrInterfaceClass = $classOrInterfaceReflection->getName();
+		} else {
+			$classOrInterfaceClass = $classOrInterface;
+		}
+
+		$madeClass = @get_class( $made );
+
+		if ( false === $madeClass ) {
+			return;
+		}
+
+		$implementationReflection = new ReflectionClass( $made );
+		$implementationClass = $implementationReflection->getName();
+
+		$classOrInterfaceClass = '\\' . ltrim( $classOrInterfaceClass, '\\' );
+		$implementationClass = '\\' . ltrim( $implementationClass, '\\' );
+
+		$this->made[ $classOrInterfaceClass ] = $implementationClass;
 	}
 }
